599dff1dd0022f3cef4cf1e0d7f87fba9d53816d
deliacirstea <deliutacirstea@gmail.com>
2022-10-25 20:13:23 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿Console.Write("Feed an X value to the program: ");
+int x = int.Parse(Console.ReadLine());
+
+Console.Write("Feed an Y value to the program: ");
+int y = int.Parse(Console.ReadLine());
+
+if (x < 0 && y > 0)
+{
+    Console.WriteLine("The enemy is in the NorthWest!");
+}
+else if (x == 0 && y > 0)
+{
+    Console.WriteLine("The enemy is in the North!");
+}
+else if (x > 0 && y > 0)
+{
+    Console.WriteLine("The enemy is in the NorthEast!");
+}
+else if (x < 0 && y == 0)
+{
+    Console.WriteLine("The enemy is in the West!");
+}
+else if (x > 0 && y == 0)
+{
+    Console.WriteLine("The enemy is in the East!");
+}
+else if (x < 0 && y < 0)
+{
+    Console.WriteLine("The enemy is in the SouthWest!");
+}
+else if (x == 0 && y < 0)
+{
+    Console.WriteLine("The enemy is in the South!");
+}
+else if (x > 0 && y < 0)
+{
+    Console.WriteLine("\nThe enemy is in the SouthEast ");
+}
+else
+{
+    Console.Beep();
+    Console.ForegroundColor = ConsoleColor.DarkBlue;
+    Console.WriteLine("\n\t\tTHE ENEMY IS HERE!");
+}
+Console.ReadLine();
